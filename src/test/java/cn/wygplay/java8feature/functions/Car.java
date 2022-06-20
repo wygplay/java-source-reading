18334dd928fdd670fe46f200e306d784647a97f0
wygplay <vipwyg@163.com>
2022-06-21 00:54:27 +0800

    supplier

--- src/test/java/cn/wygplay/java8feature/functions/Car.java
+++ src/test/java/cn/wygplay/java8feature/functions/Car.java
@@ -9,6 +9,11 @@ import java.util.Objects;
 public class Car {
     private String name;
 
+    public Car() {}
+    public Car(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
--- src/test/java/cn/wygplay/java8feature/functions/SupplierTest.java
+++ src/test/java/cn/wygplay/java8feature/functions/SupplierTest.java
@@ -1,10 +1,22 @@
 package cn.wygplay.java8feature.functions;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 /**
- * 没有参数
+ * Supplier不包含参数
  * @author wyg
  * @date 2022/6/19 22:45
  */
 public class SupplierTest {
-
+    @Test
+    public void test() {
+        Supplier<Car> carSupplier = Car::new;
+        Car car = carSupplier.get();
+        assertTrue(Objects.nonNull(car));
+    }
 }
