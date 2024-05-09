7e4470a45a26472a36a663c12306ba0ff25bd232
wygplay <vipwyg@163.com>
2024-05-09 16:14:30 +0800

    整数与字符串

---
+++ src/main/java/cn/wygplay/StringTest.java
@@ -0,0 +1,10 @@
+package cn.wygplay;
+
+
+public class StringTest {
+    public static void main(String[] args) {
+        String str = "play";
+        System.out.println(str.toLowerCase());
+        ClassLoader classLoader = StringTest.class.getClassLoader();
+    }
+}
---
+++ src/main/java/cn/wygplay/lang/IntegerTest.java
@@ -0,0 +1,11 @@
+package cn.wygplay.lang;
+
+/**
+ * @author wyg
+ * @date 2022/6/25 17:27
+ */
+public class IntegerTest {
+    public static void main(String[] args) {
+        Integer.parseInt("123");
+    }
+}
